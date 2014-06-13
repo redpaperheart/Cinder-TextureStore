e6dd79d1d7692b5f8f73d992425f39597fee175a
Daniel Scheibel <daniel@visualplayground.de>
2014-06-12 21:22:00 -0400

    Added cinderblock.xml and BasicSample

---
+++ samples/BasicSample/src/BasicSampleApp.cpp
@@ -0,0 +1,37 @@
+#include "cinder/app/AppNative.h"
+#include "cinder/gl/gl.h"
+
+#include "rph/TextureStore.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class BasicSampleApp : public AppNative {
+  public:
+	void setup();
+	void mouseDown( MouseEvent event );	
+	void update();
+	void draw();
+};
+
+void BasicSampleApp::setup()
+{
+}
+
+void BasicSampleApp::mouseDown( MouseEvent event )
+{
+}
+
+void BasicSampleApp::update()
+{
+    rph::TextureStore::getInstance()->status();
+}
+
+void BasicSampleApp::draw()
+{
+	// clear out the window with black
+	gl::clear( Color( 0, 0, 0 ) ); 
+}
+
+CINDER_APP_NATIVE( BasicSampleApp, RendererGl )
